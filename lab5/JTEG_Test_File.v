f4185a2158c44e7ea2f121c90ba8211c0757d04c
Lu Haofan <haofan.17@intl.zju.edu.cn>
2020-02-18 18:21:31 -0600

    first commit

---
+++ lab5/JTEG_Test_File.v
@@ -0,0 +1,71 @@
+`timescale 1ns / 1ps
+
+module JTEG_Test_File(
+    input   wire    [4:0] okUH,
+    output  wire    [2:0] okHU,
+    inout   wire    [31:0] okUHU,
+    inout   wire    okAA,
+    input [3:0] button,
+    output [7:0] led,
+    input sys_clkn,
+    input sys_clkp,  
+    output ADT7420_A0,
+    output ADT7420_A1,
+    output I2C_SCL_0,
+    inout I2C_SDA_0 
+);
+
+    wire  ILA_Clk, ACK_bit, FSM_Clk, TrigerEvent;    
+    wire [23:0] ClkDivThreshold = 1_000;   
+    wire SCL, SDA; 
+    wire [7:0] state;
+    wire [15:0] temp;
+    reg triger = 0;
+    reg [7:0] counter;
+    
+    assign TrigerEvent = button[3];
+    
+    always @(posedge FSM_Clk) begin 
+        if(TrigerEvent == 1'b0) triger = 1;
+    end
+    
+    always @(posedge FSM_Clk) begin 
+        if(triger == 1) begin
+            counter = counter + 1'b1;
+        end
+    end
+
+    //Instantiate the module that we like to test
+    I2C_Transmit I2C_Test1 (
+        .button(button),
+        .led(led),
+        .sys_clkn(sys_clkn),
+        .sys_clkp(sys_clkp),
+        .ADT7420_A0(ADT7420_A0),
+        .ADT7420_A1(ADT7420_A1),
+        .I2C_SCL_0(I2C_SCL_0),
+        .I2C_SDA_0(I2C_SDA_0),             
+        .FSM_Clk_reg(FSM_Clk),        
+        .ILA_Clk_reg(ILA_Clk),
+        .ACK_bit(ACK_bit),
+        .SCL(SCL),
+        .SDA(SDA),
+        .State_O(state),
+        .SDA_data(temp)
+        );
+    OK_com OK_module (
+        .okUH(okUH),
+        .okHU(okHU),
+        .okUHU(okUHU),
+        .okAA(okAA),
+        .sys_clkn(sys_clkn),
+        .sys_clkp(sys_clkp),
+        .temp(temp)
+        );
+    //Instantiate the ILA module
+    ila_0 ila_sample12 ( 
+        .clk(ILA_Clk),
+        .probe0({led, SDA, SCL, ACK_bit, state}),                             
+        .probe1({FSM_Clk, TrigerEvent})
+        );                        
+endmodule
---
+++ lab5/OK_com.v
@@ -0,0 +1,62 @@
+`timescale 1ns / 1ps
+
+module OK_com(
+        input   wire    [4:0] okUH,
+        output  wire    [2:0] okHU,
+        inout   wire    [31:0] okUHU,
+        inout   wire    okAA,
+        input   wire    sys_clkn,
+        input   wire    sys_clkp,
+        // Your signals go here
+        input wire [15:0] temp
+    );
+       
+    wire okClk;            //These are FrontPanel wires needed to IO communication    
+    wire [112:0]    okHE;  //These are FrontPanel wires needed to IO communication    
+    wire [64:0]     okEH;  //These are FrontPanel wires needed to IO communication    
+            
+    //Declare your registers or wires to send or recieve data
+    //wire [31:0] variable_1, variable_2;      //signals that are outputs from a module must be wires
+    wire [31:0] result_wire;                 //signals that go into modules can be wires or registers
+    //reg  [31:0] result_register;             //signals that go into modules can be wires or registers
+    
+    //This is the OK host that allows data to be sent or recived    
+    okHost hostIF (
+        .okUH(okUH),
+        .okHU(okHU),
+        .okUHU(okUHU),
+        .okClk(okClk),
+        .okAA(okAA),
+        .okHE(okHE),
+        .okEH(okEH)
+    );
+        
+    //Depending on the number of outgoing endpoints, adjust endPt_count accordingly.
+    //In this example, we have 2 output endpoints, hence endPt_count = 2.
+    localparam  endPt_count = 1;
+    wire [endPt_count*65-1:0] okEHx;  
+    okWireOR # (.N(endPt_count)) wireOR (okEH, okEHx);
+    
+    // Clock
+    wire clk;
+    reg [31:0] clkdiv;
+    reg slow_clk;
+    reg [7:0] counter;
+    
+    IBUFGDS osc_clk(
+        .O(clk),
+        .I(sys_clkp),
+        .IB(sys_clkn)
+    );
+    
+    assign result_wire = temp;    // Left-Side of 'assign' statement must be a 'wire'
+
+    // result_wire is transmited to the PC via address 0x20   
+    okWireOut wire20 (  .okHE(okHE), 
+                        .okEH(okEHx[ 0*65 +: 65 ]),
+                        .ep_addr(8'h20), 
+                        .ep_datain(result_wire));
+                        
+
+endmodule
+
